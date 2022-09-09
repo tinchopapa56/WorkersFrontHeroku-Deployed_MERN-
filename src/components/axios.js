99529f0a7c52667a55b13e62ecb0b5e89ed198e5
Tincho develops <martinlabastie.p@gmail.com>
2022-09-09 17:53:55 -0300

    added comment vercel route

--- src/components/axios.js
+++ src/components/axios.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
     const basePoint = axios.create({
         baseURL: "http://localhost:8080"
+        // baseURL: "https://workers-back-vercel.vercel.app"
     })
 
 export default basePoint
